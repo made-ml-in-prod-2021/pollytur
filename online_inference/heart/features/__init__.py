ee89cdd49000666487c5c0895799340f9ad65d4a
PollyTur <p.turischeva@innopolis.university>
2021-05-19 21:49:15 +0300

    hw2

---
+++ online_inference/heart/features/__init__.py
@@ -0,0 +1,1 @@
+from .build_features import preprocess_dataset, CustomTransformer, load_transformer, save_transformer
---
+++ online_inference/tickle.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d @request.json localhost:5000/predict
